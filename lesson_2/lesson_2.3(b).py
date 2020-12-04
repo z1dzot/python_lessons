b9f85e732c9259d9b51e561cf8a62a8abc76ec8a
Kael <z1dzot@gmail.com>
2020-12-04 08:19:45 +0300

    HW 2 UPD SEASONS

--- lesson_2/lesson_2.3(b).py
+++ lesson_2/lesson_2.3(b).py
@@ -27,4 +27,38 @@ while month_number in seasons:
         print('Это Осень!')
         break
 
+# seasons_dict = {
+#         1: "winter",
+#         2: "winter",
+#         3: "spring",
+#         4: "spring",
+#         5: "spring",
+#         6: "summer",
+#         7: "summer",
+#         8: "summer",
+#         9: "autumn",
+#         10: "autumn",
+#         11: "autumn",
+#         12: "winter"}
+#
+# seasons_list = ["winter",
+#                 "winter",
+#                 "spring",
+#                 "spring",
+#                 "spring",
+#                 "summer",
+#                 "summer",
+#                 "summer",
+#                 "autumn",
+#                 "autumn",
+#                 "autumn",
+#                 "winter"]
+#
+# try:
+#     month = int(input("Enter the month: "))
+#     print(f"FROM DICT: Month {month} is {seasons_dict[month]} month")
+#     print(f"FROM LIST: Month {month} is {seasons_list[month - 1]} month")
+# except ValueError as err:
+#     print("Wrong value has been entered!", err)
+
 
